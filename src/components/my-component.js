8563dab805b198ff21e969c34d3de52940680c35
Qiu Yong Hao <qwingho@gmail.com>
2021-10-21 21:53:04 -0700

    Finished Gatsby Tutorial Part 5

--- src/components/my-component.js
+++ src/components/my-component.js
@@ -1,12 +1,8 @@
-import * as React from 'react';
-import { title } from './my-component.module.css';
+import * as React from "react";
+import { title } from "./my-component.module.css";
 
 const MyComponent = () => {
-    return (
-        <h1 className={title}>
-            Super Sweet Title Page
-        </h1>
-    )
-}
+  return <h1 className={title}>Super Sweet Title Page</h1>;
+};
 
 export default MyComponent;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
-import * as React from 'react';
-import Layout from '../components/layout';
-import { StaticImage } from 'gatsby-plugin-image';
+import * as React from "react";
+import Layout from "../components/layout";
+import { StaticImage } from "gatsby-plugin-image";
 
 const IndexPage = () => {
   return (
@@ -11,7 +11,7 @@ const IndexPage = () => {
         src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
       />
     </Layout>
-  )
-}
+  );
+};
 
 export default IndexPage;
